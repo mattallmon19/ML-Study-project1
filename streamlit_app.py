da8f06dbd1303dbea7a86caaac9443fbcc114bfd
mattallmon19 <mattallmon19@gmail.com>
2026-07-27 16:32:12 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Machine Learning Project 1')
 
 st.write('This app is built to use machine learning to predict the price of gold using machine learning!')
+
+with st.expander( 'Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv(...)
+  df
+
+  st.write('**X**')
+  X = df.drop('Metal', axis=1)
